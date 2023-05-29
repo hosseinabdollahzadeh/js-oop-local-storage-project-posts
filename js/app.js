d3da5714bd5d8a3a94a452847b7033661c6fe4a4
hossein <abdollahzadeh.hossein@gmail.com>
2023-05-29 15:27:07 +0330

    Validation alert

--- js/app.js
+++ js/app.js
@@ -26,6 +26,32 @@ class UI {
         list.appendChild(row);
     }
 
+    showAlert(message, className) {
+
+        // Create div
+        const div = document.createElement('div');
+
+        // Add classes
+        div.className = `alert alert-${className}`;
+
+        // Add text
+        div.appendChild(document.createTextNode(message));
+
+        // Get parent
+        const col = document.querySelector('.col-sm-8');
+
+        // Get form
+        const form = document.querySelector('#post-form');
+
+        // Insert alert
+        col.insertBefore(div, form);
+
+        // Timeout after 3 seconds
+        setTimeout(function () {
+            document.querySelector('.alert').remove();
+        }, 3000);
+    }
+
     clearFields() {
         document.getElementById('title').value = '';
         document.getElementById('author').value = '';
@@ -41,17 +67,25 @@ document.getElementById('post-form').addEventListener('submit', function (e) {
     const author = document.getElementById('author').value;
     const body = document.getElementById('body').value;
 
-    // Instantiate Post
-    const post = new Post(title, author, body);
-
     // Instantiate UI
     const ui = new UI();
 
-    // Add post to list
-    ui.addPostToList(post);
+    // Validate
+    if (title === '' || author === '' || body === '') {
+        // Error alert
+        ui.showAlert('تمام فیلدها الزامی هستند', 'danger');
+    } else {
+        // Instantiate Post
+        const post = new Post(title, author, body);
 
-    // Clear fields
-    ui.clearFields();
+        // Add post to list
+        ui.addPostToList(post);
+
+        ui.showAlert('پست اضافه شد', 'success');
+
+        // Clear fields
+        ui.clearFields();
+    }
 
     e.preventDefault();
 });
